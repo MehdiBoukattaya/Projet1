005845a5ba92bb2cecfebea2efd0232861718655
Mehdibkt <boukattayamehdi99@gmail.com>
2023-06-11 23:02:06 -0400

    timer swiper

--- index.php
+++ index.php
@@ -166,7 +166,7 @@
             effect: "fade",
             loop: true,
             autoplay: {
-                delay: 2000,
+                delay: 1000,
                 disableOnInteraction: false,
             }
         });
